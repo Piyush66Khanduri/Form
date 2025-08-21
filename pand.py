0eb6a725355133f31ace4fd18ec2943f1a8b47e9
Piyush Khanduri <piyushkhanduri65@gmail.com>
2025-08-21 20:21:16 +0530

    All files working

--- pand.py
+++ pand.py
@@ -1,3 +1,3 @@
 import pandas as pd
-df=pd.DataFrame({""["Piyush","Karan","Ram","Laxman"],"Roll Numbers":[1,2,3,4]})
+df=pd.DataFrame({"Name":["Piyush","Karan","Ram","Laxman"],"Roll Numbers":[1,2,3,4]})
 print(df)
